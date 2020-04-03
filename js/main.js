80d4454753169d85cda25ed709efee094e90ac76
Abdelrhman Gad <abdelrhman.gad2015@yahoo.com>
2020-04-03 12:57:39 +0200

    Adding Js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,25 @@ const closeBtn = document.getElementById("closeBtn");
 const asideMenu = document.getElementById('aside');
 const dropMenu = document.getElementById('dropMenu');
 
-console.log(asideMenu.offsetWidth);
+asideMenu.style.transform = "translateX("+ asideMenu.offsetWidth +"px)";
 
 function closeMenu(){
-    asideMenu.style.transform = `translateX(${asideMenu.offsetWidth}px)`;
-    console.log("Helo")
+    asideMenu.style.transform = "translateX("+ asideMenu.offsetWidth +"px)";
 };
+console.log(asideMenu.offsetWidth)
+console.log(asideMenu)
 
 function openMenu(){
-    asideMenu.style.transform = `translateX(0px)`;
+    asideMenu.style.transform = "translateX(0px)";
 }
+
+console.log("translateX(-"+ asideMenu.offsetWidth +"px)")
+console.log("translateX("+ asideMenu.offsetWidth +"px)")
+
+dropMenu.addEventListener("click", function(){
+    console.log("CLICKED")
+    console.log("translateX(-"+ asideMenu.offsetWidth +")");
+    asideMenu.style.transform = "translateX(-"+ asideMenu.offsetWidth +")";
+})
 
 // End  ASide menu Closing
