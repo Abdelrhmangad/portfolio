bb79c45749ede8d8ec2780fa77d9b148bdbfc166
Abdelrhmangad <abdelrhman.gad2015@yahoo.com>
2020-10-26 13:43:28 +0200

    Adding my resume and more porjects, and make my resume downloadable

--- js/project.mvc.js
+++ js/project.mvc.js
@@ -3,6 +3,11 @@
 let model = {
    projects: [
       {
+         name: "Crown Shop",
+         url: "https://crownclothesreactapp.herokuapp.com/",
+         img: "images/crown-commerce.png"
+      },
+      {
          name: "Clinc",
          url: "https://abdelrhmangad.github.io/Clinc/#",
          img: "images/med1.png"
@@ -18,12 +23,18 @@ let model = {
          img: "images/ax1.png"
       },
       {
-         name: "Bino.INC",
-         url: "https://abdelrhmangad.github.io/Bino/",
-         img: "images/bin1.png"
+         name: "Online Grocery",
+         url: "https://gad-ecommerce-app.herokuapp.com/",
+         img: "images/gad-ecommerce.png"
+      },
+      {
+         name: "Monster Rolodex",
+         url: "https://abdelrhmangad.github.io/Monster-Rolodex_ReactProject/",
+         img: "images/monster-rolodex.png"
       }
    ]
 };
+
 
 let controller = {
    init: function () {
@@ -33,6 +44,8 @@ let controller = {
       return model.projects
    }
 };
+
+
 
 let viewProjects = {
    init: function () {
@@ -47,7 +60,7 @@ let viewProjects = {
                   <div class="demo-div">
                      <a href="${project.url}" target="_blank">View Demo</a> 
                   </div>
-                  <img src="${project.img}" alt="">
+                  <img src="${project.img}" alt=" Sorry Image Didn't load well">
                </div>
                <p class="project-name">
                   ${project.name}
